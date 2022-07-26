36015495ce4b00145a44ec6bf6e07422cb9ff1f2
y3owk1n <wongyeowkin@gmail.com>
2022-07-26 19:27:55 +0800

    added simple umami tracker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import { AppProps } from "next/app";
+import Script from "next/script";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const isDev = process.env.NODE_ENV === "development";
+  return (
+    <>
+      {!isDev && (
+        <Script
+          src={"https://kyle-umami.up.railway.app/umami.js"}
+          data-website-id="2732127c-366b-4a69-8067-b750b856bbd4"
+          strategy="lazyOnload"
+        />
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
